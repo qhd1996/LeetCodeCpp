85bad229b67986a5a7146b4233b3ac8109fed68a
qhd0081 <qhd0081@vip.163.com>
2019-07-21 22:07:04 +0800

    Update Two Sum.cpp

--- LeetCodeCpp/Two Sum.cpp
+++ LeetCodeCpp/Two Sum.cpp
@@ -1,21 +1,23 @@
 //Hash Table
 //Time Complexity O(n)
-class Solution {
+//Space Complexity O(n)
+
+class Solution{
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		unordered_map<int, int> mapping;
-		vector<int> result;
-		for (int i = 0; i < nums.size(); i++) {
-			mapping[nums[i]] = i;
-		}
-		for (int i = 0; i < nums.size(); i++) {
-			const int gap = target - nums[i];
-			if (mapping.find(gap) != mapping.end() && mapping[gap] > i) {
-				result.push_back(i);
-				result.push_back(mapping[gap]);
-				break;
-			}
-		}
-		return result;
-	}
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mapping;
+        vector<int> result;
+        for(int i = 0; i<nums.size(); i++){
+            mapping[nums[i]] = i;
+        }
+        for(int i =0; i<nums.size(); i++){
+            const int difference = target - nums[i];
+            if(mapping.find(difference) != mapping.end() &&         mapping[difference]>i){
+                result.push_back(i);
+                result.push_back(mapping[difference]);
+                break;
+            }
+        }
+        return result;
+    }
 };
