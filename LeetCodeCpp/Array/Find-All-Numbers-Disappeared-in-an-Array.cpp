a8e031c605633f5a2d32e1a252f866c398482ba2
qhd0081 <qhd0081@vip.163.com>
2019-10-04 16:47:51 +0800

    Update Find-All-Numbers-Disappeared-in-an-Array.cpp

--- LeetCodeCpp/Array/Find-All-Numbers-Disappeared-in-an-Array.cpp
+++ LeetCodeCpp/Array/Find-All-Numbers-Disappeared-in-an-Array.cpp
@@ -1,27 +1,29 @@
-//Hash Table
+/**
+This problem does not allow sorting or hashtable, and the answer is quite clever.
+First we iterate through the input array and mark elements as negative using nums[nums[i] -1] = -nums[nums[i]-1](only change once). 
+In this way all the numbers that we have seen will be marked as negative. 
+In the second iteration, if a value is not marked as negative,  it implies we have never seen that index before, so just add it to the return list.
+**/
+//Index
 //Time Complexity O(n)
-//Space Complexity O(n)
+//Space Complexity O(1)
 
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> key(nums.size() + 1, 0);
-    
         for(int i = 0; i < nums.size(); i++){
-            key[nums[i]]++;
+            int val = abs(nums[i]) - 1;
+            if(nums[val] > 0)
+                nums[val] = -nums[val];
         }
         
-        int smallest = 1;
-        int largest = nums.size();
-        vector<int> missing;
-    
-        for(int i = smallest; i <= largest; i++){
-            if(key[i] == 0){
-             missing.push_back(i);
-            }
-        }
-    return missing;
+        vector<int> returnList;
         
+        for(int j = 0; j < nums.size(); j++){
+           if(nums[j] > 0)
+               returnList.push_back(j + 1);
+        }
+        
+        return returnList;
     }
-    
 };
