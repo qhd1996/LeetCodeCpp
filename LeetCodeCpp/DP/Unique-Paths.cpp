667703ee3909e13f06cdac664f4bb94efdf227f4
qhd0081 <qhd0081@vip.163.com>
2019-10-30 23:41:00 +0800

    Create Unique-Paths.cpp

---
+++ LeetCodeCpp/DP/Unique-Paths.cpp
@@ -0,0 +1,61 @@
+//Solution1
+//DP
+//Time Complexity O(m * n)
+//Space Complexity O(m * n)
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int> > dp(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+//Solution2
+//DP
+//Time Complexity O(m * n)
+//Space Complexity O(n)
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> cur(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+};
+
+//Solution3
+//Math $\frac{(m + n)!}{m!n!}$
+//Time complexity O(max(m, n))
+//Space Complexity O(1)
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long  res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
+    }
+        
+};
