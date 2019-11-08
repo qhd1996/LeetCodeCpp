ccba27790c77bcf0512567227daf864b70962ac3
qhd0081 <qhd0081@vip.163.com>
2019-11-08 22:41:32 +0800

    Create Combination-Sum-III.cpp

---
+++ LeetCodeCpp/DFS/Combination-Sum-III.cpp
@@ -0,0 +1,38 @@
+//Array, DFS
+//Time Complexity O(n ^ 2)
+//Space Complexity O(n ^ 2)
+class Solution {
+public: 
+    void search(int len, int target, vector<int>&nums, int start, vector<int>& tmp, vector<vector<int> >& res){
+        
+        if(tmp.size() == len && target == 0){
+            res.push_back(tmp);
+            return;
+        }
+        
+        if(tmp.size()>len || target<0){
+            return;
+        }
+        
+        for(int i  = start; i < nums.size(); i++){
+            tmp.push_back(nums[i]);
+            search(len, target - nums[i], nums, i + 1, tmp, res);
+            tmp.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>nums = {1,2,3,4,5,6,7,8,9};
+        vector<vector<int> > res;
+        
+        //Corner case
+        if(k == 0) return res;
+        
+        //General case
+        vector<int> tmp;
+        search(k, n, nums, 0, tmp, res);
+        
+        return res;
+        
+    }
+};
