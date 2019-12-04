a07d1d2a111a6471bad0951f7ae4be0119124cc0
qhd0081 <qhd0081@vip.163.com>
2019-12-04 23:58:47 +0800

    Create Simplify-Path.cpp

---
+++ LeetCodeCpp/String/Simplify-Path.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        deque<string> myDeque;
+        istringstream ss(path);
+        string str;
+        while (getline(ss, str, '/')) 
+            if (!str.empty()) {
+                if (str == ".." && !myDeque.empty())
+                    myDeque.pop_back();
+                else if (str != "." && str != "..")
+                    myDeque.push_back(str);
+            }
+        string res = "";
+        while (!myDeque.empty()) {
+            res += "/" + myDeque.front();
+            myDeque.pop_front();
+        }
+        return res.empty()?"/":res;
+    }
+};
