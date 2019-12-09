9eb651a9ec75af8d4f605800d62fdc4aa7866623
qhd0081 <qhd0081@vip.163.com>
2019-12-09 23:16:46 +0800

    Create Letter Combinations-of-a-Phone-Number.cpp

---
+++ LeetCodeCpp/Math/Letter Combinations-of-a-Phone-Number.cpp
@@ -0,0 +1,27 @@
+//DFS
+//Time Complexity O(3 ^ n or 4 ^ n)
+//Space Complexity O(n)
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string>res;
+        if(digits.empty()) return res;
+        vector<string>letter({"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"});
+        string path = "";
+        DFS(digits, 0, path, res, letter);
+        return res;
+    }
+    
+    void DFS(string digits, int pos, string& path, vector<string>& res, vector<string>& letter){
+        if(pos == digits.size()){
+            res.push_back(path);
+            return;
+        }
+        for(auto c: letter[digits[pos] - '0']){
+            path.push_back(c);
+            DFS(digits, pos + 1, path, res, letter);
+            path.pop_back();
+        }
+    }
+};
