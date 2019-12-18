83317a753157bfae003bab310df0f7949c67c7ff
qhd0081 <qhd0081@vip.163.com>
2019-12-18 23:51:48 +0800

    Create Length-of-Last-Word.cpp

---
+++ LeetCodeCpp/String/Length-of-Last-Word.cpp
@@ -0,0 +1,16 @@
+//String
+//Time Complexity O(n)
+//Space Complexity O(1)
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) { 
+        int len = 0, tail = s.length() - 1;
+        while (tail >= 0 && s[tail] == ' ') tail--;
+        while (tail >= 0 && s[tail] != ' ') {
+            len++;
+            tail--;
+        }
+        return len;
+    }
+};
