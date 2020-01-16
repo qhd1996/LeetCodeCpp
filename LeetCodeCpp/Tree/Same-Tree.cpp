6e50994b3ec8bef8d7b7d94f7530cfbdc12b3f05
qhd0081 <qhd0081@vip.163.com>
2020-01-16 21:40:10 +0800

    Create Same-Tree.cpp

---
+++ LeetCodeCpp/Tree/Same-Tree.cpp
@@ -0,0 +1,30 @@
+//Time Complexity O(n)
+//Space Complexity O(logn)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q != NULL || p != NULL && q == NULL)
+            return false;
+        else if(p == NULL && q == NULL)
+            return true;
+        else if(p->val != q->val)
+            return false;
+        else if (isSameTree(p->left, q->left) == false)
+            return false;
+        else if (isSameTree(p->right, q->right) == false)
+            return false;
+        else
+            return true;
+        
+    }
+};
