327d13e8ed0c1518ac9becf31c6fa43d5393e29a
qhd0081 <qhd0081@vip.163.com>
2020-01-18 00:40:29 +0800

    Create Maximum-Depth-of-Binary-Tree.cpp

---
+++ LeetCodeCpp/Tree/Maximum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,18 @@
+//Tree, DFS
+//Time Complexity O(n)
+//Space Complexity O(logn)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return root ? 1 + max(maxDepth(root->left), maxDepth(root->right)) : 0;
+    }
+};
