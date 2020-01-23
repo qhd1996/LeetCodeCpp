636c5cc60b7876a46993ce4d4c0a2b2a22a0d54a
qhd0081 <qhd0081@vip.163.com>
2020-01-23 23:17:10 +0800

    Create Invert-Binary-Tree.cpp

---
+++ LeetCodeCpp/Tree/Invert-Binary-Tree.cpp
@@ -0,0 +1,42 @@
+//DFS
+//Time Complexity O(n)
+//Space Complexity O(logn)
+
+//non-recursive
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+       TreeNode* invertTree(TreeNode* root) {
+        if (root) {
+            invertTree(root->left);
+            invertTree(root->right);
+            std::swap(root->left, root->right);
+        }
+        return root;
+    }
+};
+
+//recursive
+TreeNode* invertTree(TreeNode* root) {
+    std::stack<TreeNode*> stk;
+    stk.push(root);
+    
+    while (!stk.empty()) {
+        TreeNode* p = stk.top();
+        stk.pop();
+        if (p) {
+            stk.push(p->left);
+            stk.push(p->right);
+            std::swap(p->left, p->right);
+        }
+    }
+    return root;
+}
