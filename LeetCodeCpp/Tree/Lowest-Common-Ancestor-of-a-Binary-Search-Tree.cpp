90cddc96a10cecc3a9a9b00895daecc9a21ecac0
qhd0081 <qhd0081@vip.163.com>
2020-01-24 09:06:25 +0800

    Create Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp

---
+++ LeetCodeCpp/Tree/Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
@@ -0,0 +1,31 @@
+//Tree
+//Time Complexity O(n)
+//Space Complexitu
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root) {
+            if (p->val > root->val && q->val > root->val) {
+                root = root->right;
+                continue;
+            }
+            if (p->val < root->val && q->val < root->val) {
+                root = root->left;
+                continue;
+            }
+            return root;
+            
+        }
+        return NULL;
+        
+    }
+};
